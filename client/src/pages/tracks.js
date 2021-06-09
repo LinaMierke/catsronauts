1aa9cf21cf1c8739715e2d76a64b5742b03484b4
lina mierkE <lina_maria89@hotmail.com>
2021-06-09 18:56:35 -0400

    copied operations

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,6 +1,23 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import { Layout } from "../components";
+import { gql } from "@apollo/client";
 
+const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`;
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
