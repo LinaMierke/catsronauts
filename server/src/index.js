990eadbebbbb7c783f51dc8a3aaa28d481bd215d
lina mierkE <lina_maria89@hotmail.com>
2021-06-09 11:17:28 -0400

    index js

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,13 @@
-//TODO
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(() => {
+  console.log(
+    `
+        Server is running!
+        Listenning on port 4000
+        Query at https://studio.apollographql.com/dev`
+  );
+});
--- server/src/schema.js
+++ server/src/schema.js
@@ -9,6 +9,15 @@ const typeDefs = gql`
     length: Int
     modulesCount: Int
   }
+  type Author {
+      id: ID!
+      name: String!
+      photo: String
+  }
+  type Query {
+      "Get tracks array for homepage grid"
+      tracksForHome: [Track!]
+  }
 `;
 
 module.exports = typeDefs;
