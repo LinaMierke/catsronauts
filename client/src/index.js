218ce5f48c73cb42c012a9e590693b4f004253c5
lina mierkE <lina_maria89@hotmail.com>
2021-06-10 13:30:50 -0400

    bug oh bug

--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,15 @@ import Pages from "./pages";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: "http://localhost:3000",
   cache: new InMemoryCache(),
 });
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <GlobalStyles />
-    <Pages />
-  </ApolloProvider>,
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Layout } from "../components";
 import { useQuery, gql } from "@apollo/client";
-
+import TrackCard from "../containers/track-card";
 const TRACKS = gql`
   query getTracks {
     tracksForHome {
@@ -21,9 +21,18 @@ const TRACKS = gql`
 
 const Tracks = () => {
   const { loading, error, data } = useQuery(TRACKS);
+
   if (loading) return "Loading...";
-  if (error) return `Error! ${error.message}`;
-  return <Layout grid>{JSON.stringify(data)}</Layout>;
+
+  if (error) return `ERROR! ${error.message}`;
+
+  return (
+    <Layout grid>
+      {data?.tracksForHome?.map((track) => (
+        <TrackCard key={track.id} track={track} />
+      ))}
+    </Layout>
+  );
 };
 
 export default Tracks;
